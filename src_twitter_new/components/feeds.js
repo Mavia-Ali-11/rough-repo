c3b7c1049b30b5881958c1e6860bfe5713c24464
Mavia Ali <maviaali597@gmail.com>
2022-01-24 01:29:43 +0500

    feeds changes

--- src_twitter_new/components/feeds.js
+++ src_twitter_new/components/feeds.js
@@ -13,7 +13,7 @@ function Feeds(props) {
     const [fetchedTweets, handleFetchedTweets] = useState([]);
     const [fetchedReactions, handleFetchedReactions] = useState([]);
     const [isDisbaled, handleDisability] = useState(true);
-    const [btnAccess, handleBtnAccess] = useState(0.6);
+    const [btnAccess, handleBtnAccess] = useState(0.5);
     const [showEmojis, setShowEmojis] = useState(true);
 
     const onEmojiClick = (event, emojiObject) => {
@@ -281,10 +281,13 @@ function Feeds(props) {
 
     return (
         <div className='feeds'>
-            <div className='hiddenDOM' onClick={() => { setShowEmojis(true) }} hidden={showEmojis}></div>
+            <div className='hiddenDOM' onScroll={() => { setShowEmojis(true) }} hidden={showEmojis}></div>
 
-            <div className='header'>
+            <div className='header' onClick={()=>{document.getElementsByClassName('feeds')[0].scrollTop = 0}}>
                 <h4>{props.scrName}</h4>
+                <div>
+                    <svg viewBox="0 0 24 24" aria-hidden="true"><g><path d="M22.772 10.506l-5.618-2.192-2.16-6.5c-.102-.307-.39-.514-.712-.514s-.61.207-.712.513l-2.16 6.5-5.62 2.192c-.287.112-.477.39-.477.7s.19.585.478.698l5.62 2.192 2.16 6.5c.102.306.39.513.712.513s.61-.207.712-.513l2.16-6.5 5.62-2.192c.287-.112.477-.39.477-.7s-.19-.585-.478-.697zm-6.49 2.32c-.208.08-.37.25-.44.46l-1.56 4.695-1.56-4.693c-.07-.21-.23-.38-.438-.462l-4.155-1.62 4.154-1.622c.208-.08.37-.25.44-.462l1.56-4.693 1.56 4.694c.07.212.23.382.438.463l4.155 1.62-4.155 1.622zM6.663 3.812h-1.88V2.05c0-.414-.337-.75-.75-.75s-.75.336-.75.75v1.762H1.5c-.414 0-.75.336-.75.75s.336.75.75.75h1.782v1.762c0 .414.336.75.75.75s.75-.336.75-.75V5.312h1.88c.415 0 .75-.336.75-.75s-.335-.75-.75-.75zm2.535 15.622h-1.1v-1.016c0-.414-.335-.75-.75-.75s-.75.336-.75.75v1.016H5.57c-.414 0-.75.336-.75.75s.336.75.75.75H6.6v1.016c0 .414.335.75.75.75s.75-.336.75-.75v-1.016h1.098c.414 0 .75-.336.75-.75s-.336-.75-.75-.75z"></path></g></svg>
+                </div>
             </div>
 
             <div className='tweet-maker'>
@@ -306,7 +309,7 @@ function Feeds(props) {
                                 handleDisability(false);
                                 handleBtnAccess(1);
                             } else if (e.target.value.length == 0 || e.target.value.length > 280) {
-                                handleBtnAccess(0.6);
+                                handleBtnAccess(0.5);
                                 handleDisability(true);
                                 if (e.target.value.length > 280) {
                                     handleTweet(tweet);
@@ -384,7 +387,7 @@ function Feeds(props) {
                                             tweet_time: time,
                                             tweet_text: tweet,
                                             uid: state.authUser.uid,
-                                            tweet_avatar: state.authUser.avatar, 
+                                            tweet_avatar: state.authUser.avatar,
                                             tweet_from: state.authUser.email,
                                             tweet_by: state.authUser.username,
                                             tweet_counter: tweetsCounter.data().counter,
@@ -411,38 +414,44 @@ function Feeds(props) {
                         let dislikedData = [];
                         return (
                             <div key={index}>
-                                <div className="postData">
+                                <div className="post-data">
                                     <div>
                                         <img src={tweet.tweet_avatar} />
                                     </div>
                                     <div>
-                                        <div className='tweetMeta'>
-                                            <h4>{tweet.tweet_by}</h4>
-                                            <p>{tweet.tweet_from}</p>
-                                            <p>{tweet.tweet_date}</p>
-                                            <p>{tweet.tweet_time}</p>
-                                            <p>{tweet.timestamp}</p>
+                                        <div className='tweet-meta'>
+                                            <div>
+                                                <h4>{tweet.tweet_by}</h4>
+                                                <p>{tweet.tweet_from}</p>
+                                                <p>{tweet.tweet_date}</p>
+                                                <p>{tweet.tweet_time}</p>
+                                                <p>{tweet.timestamp}</p>
+                                            </div>
+                                            <div>
+                                                <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true" data-testid="MoreHorizIcon"><path d="M6 10c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2zm-6 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2z"></path></svg>
+                                            </div>
                                         </div>
                                         <div>
                                             <p>{tweet.tweet_text}</p>
+                                        </div>
+                                        <div>
+                                            {fetchedReactions.map((reaction) => {
+                                                if (tweet.tweet_id == reaction.tweet_id && reaction[state.authUser.uid] == "liked") {
+                                                    likedData.push(true);
+                                                } else if (tweet.tweet_id == reaction.tweet_id && reaction[state.authUser.uid] == "disliked") {
+                                                    dislikedData.push(true);
+                                                } else {
+                                                    likedData.push(false);
+                                                }
+                                            })}
+                                            {decider(likedData, dislikedData, tweet.tweet_id)}
                                         </div>
                                     </div>
                                 </div>
 
 
-                                {
-                                    fetchedReactions.map((reaction) => {
-                                        if (tweet.tweet_id == reaction.tweet_id && reaction[state.authUser.uid] == "liked") {
-                                            likedData.push(true);
-                                        } else if (tweet.tweet_id == reaction.tweet_id && reaction[state.authUser.uid] == "disliked") {
-                                            dislikedData.push(true);
-                                        } else {
-                                            likedData.push(false);
-                                        }
-                                    })
-                                }
 
-                                {decider(likedData, dislikedData, tweet.tweet_id)}
+
                             </div>
                         )
                     })
